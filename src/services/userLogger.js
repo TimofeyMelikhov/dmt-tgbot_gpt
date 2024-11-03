53dda35c17de44311554b0b67896ccfab65397ec
Tima <Schibatuch@gmail.com>
2024-11-03 13:07:25 +0300

    add logs

--- src/services/userLogger.js
+++ src/services/userLogger.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 
 const LOG_FILE_PATH = "./logs/userLogs.json";
+// const LOG_FILE_PATH = "./dmt-tgbot_gpt/logs/userLogs.json";
 
 const initializeLogFile = () => {
   if (!fs.existsSync(LOG_FILE_PATH)) {
@@ -13,6 +14,8 @@ const initializeLogFile = () => {
 
 export const logUser = (userName) => {
   initializeLogFile();
+  console.log("Current working directory:", process.cwd());
+  console.log("Expected log file path:", LOG_FILE_PATH);
 
   const logData = JSON.parse(fs.readFileSync(LOG_FILE_PATH, "utf8"));
 
